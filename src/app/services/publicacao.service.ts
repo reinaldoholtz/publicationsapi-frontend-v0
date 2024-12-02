750dce1d025a067fb0cb6af687c27fb06a37d6c5
reinaldoholtz <reinaldo.holtz@gmail.com>
2024-12-02 15:06:07 +0100

    removido o api config pra usar o enviroment

--- src/app/services/publicacao.service.ts
+++ src/app/services/publicacao.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { API_CONFIG } from '../config/api.config';
 import { Publicacao } from '../models/publicacao';
 import { PublicacaoPage } from '../models/publicacao-page';
 import { environment } from 'src/environments/environment';
